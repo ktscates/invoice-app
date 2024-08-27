af9acf234325bf467373e925871107c8628ffc74
ktscates <cates2506@gmail.com>
2024-08-27 21:20:33 +0200

    empty invoices ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { EmptyInvoicesComponent } from './components/empty-invoices/empty-invoices.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [
+    RouterOutlet,
+    SidebarComponent,
+    EmptyInvoicesComponent,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/empty-invoices/empty-invoices.component.ts
+++ src/app/components/empty-invoices/empty-invoices.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { IconsComponent } from '../icons/icons.component';
 
 @Component({
   selector: 'app-empty-invoices',
   standalone: true,
-  imports: [],
+  imports: [IconsComponent],
   templateUrl: './empty-invoices.component.html',
-  styleUrl: './empty-invoices.component.css'
+  styleUrl: './empty-invoices.component.css',
 })
-export class EmptyInvoicesComponent {
-
-}
+export class EmptyInvoicesComponent {}
